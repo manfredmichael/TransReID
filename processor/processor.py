2dde7b9823d770d55ecc3e3c6c83981e44d25fc1
manfred michael <koskosanrider@gmail.com>
2022-10-12 07:22:49 +0700

    Add more metrics

--- processor/processor.py
+++ processor/processor.py
@@ -63,7 +63,6 @@ def do_train(cfg,
         mlflow.pytorch.log_model(model, cfg.MODEL.PRETRAIN_PATH)
 
         for epoch in range(1, epochs + 1):
-            start_time = time.time()
             loss_meter.reset()
             acc_meter.reset()
             evaluator.reset()
@@ -71,6 +70,7 @@ def do_train(cfg,
             model.train()
             logger.info("epoch")
             for n_iter, (img, vid, target_cam, target_view) in enumerate(train_loader):
+                start_time = time.time()
                 steps+=1
 
                 optimizer.zero_grad()
@@ -101,8 +101,6 @@ def do_train(cfg,
                 loss_meter.update(loss.item(), img.shape[0])
                 acc_meter.update(acc, 1)
 
-                mlflow.log_metric(key="train/accuracy", value=acc, step=steps)
-                mlflow.log_metric(key="train/loss", value=loss.item(), step=steps)
 
                 torch.cuda.synchronize()
                 if (n_iter + 1) % log_period == 0:
@@ -110,59 +108,56 @@ def do_train(cfg,
                                 .format(epoch, (n_iter + 1), len(train_loader),
                                         loss_meter.avg, acc_meter.avg, scheduler._get_lr(epoch)[0]))
 
-            end_time = time.time()
-            time_per_batch = (end_time - start_time) / (n_iter + 1)
-            if cfg.MODEL.DIST_TRAIN:
-                pass
-            else:
-                logger.info("Epoch {} done. Time per batch: {:.3f}[s] Speed: {:.1f}[samples/s]"
-                        .format(epoch, time_per_batch, train_loader.batch_size / time_per_batch))
+                end_time = time.time()
+                time_per_batch = (end_time - start_time)
+                # if cfg.MODEL.DIST_TRAIN:
+                #     pass
+                # else:
+                #     logger.info("Epoch {} done. Time per batch: {:.3f}[s] Speed: {:.1f}[samples/s]"
+                #             .format(epoch, time_per_batch, train_loader.batch_size / time_per_batch))
 
-            if epoch % checkpoint_period == 0:
-                if cfg.MODEL.DIST_TRAIN:
-                    if dist.get_rank() == 0:
-                        torch.save(model.state_dict(),
-                                   os.path.join(cfg.OUTPUT_DIR, cfg.MODEL.NAME + '_{}.pth'.format(epoch)))
-                else:
-                    torch.save(model.state_dict(),
-                               os.path.join(cfg.OUTPUT_DIR, cfg.MODEL.NAME + '_{}.pth'.format(epoch)))
+                mlflow.log_metric(key="train/accuracy", value=acc, step=steps)
+                mlflow.log_metric(key="train/loss", value=loss.item(), step=steps)
+                mlflow.log_metric(key="utils/time_per_batch", value=time_per_batch, step=steps)
 
-            if epoch % eval_period == 0:
+            # if epoch % checkpoint_period == 0:
+            #     if cfg.MODEL.DIST_TRAIN:
+            #         if dist.get_rank() == 0:
+            #             torch.save(model.state_dict(),
+            #                        os.path.join(cfg.OUTPUT_DIR, cfg.MODEL.NAME + '_{}.pth'.format(epoch)))
+            #     else:
+            #         torch.save(model.state_dict(),
+            #                    os.path.join(cfg.OUTPUT_DIR, cfg.MODEL.NAME + '_{}.pth'.format(epoch)))
+
+            # if epoch % eval_period == 0:
                 if cfg.MODEL.DIST_TRAIN:
-                    if dist.get_rank() == 0:
-                        model.eval()
-                        for n_iter, (img, vid, camid, camids, target_view, _) in enumerate(val_loader):
-                            with torch.no_grad():
-                                img = img.to(device)
-                                camids = camids.to(device)
-                                target_view = target_view.to(device)
-                                feat = model(img, cam_label=camids, view_label=target_view)
-                                evaluator.update((feat, vid, camid))
-                        cmc, mAP, _, _, _, _, _ = evaluator.compute()
-                        logger.info("Validation Results - Epoch: {}".format(epoch))
-                        logger.info("mAP: {:.1%}".format(mAP))
-                        mlflow.log_metric(key="val/CMC", value=cmc, step=steps)
-                        mlflow.log_metric(key="val/mAP", value=mAP, step=steps)
-                        for r in [1, 5, 10]:
-                            logger.info("CMC curve, Rank-{:<3}:{:.1%}".format(r, cmc[r - 1]))
-                        torch.cuda.empty_cache()
-                else:
-                    model.eval()
-                    for n_iter, (img, vid, camid, camids, target_view, _) in enumerate(val_loader):
-                        with torch.no_grad():
-                            img = img.to(device)
-                            camids = camids.to(device)
-                            target_view = target_view.to(device)
-                            feat = model(img, cam_label=camids, view_label=target_view)
-                            evaluator.update((feat, vid, camid))
-                    cmc, mAP, _, _, _, _, _ = evaluator.compute()
-                    logger.info("Validation Results - Epoch: {}".format(epoch))
-                    logger.info("mAP: {:.1%}".format(mAP))
-                    mlflow.log_metric(key="val/CMC", value=cmc, step=steps)
-                    mlflow.log_metric(key="val/mAP", value=mAP, step=steps)
-                    for r in [1, 5, 10]:
-                        logger.info("CMC curve, Rank-{:<3}:{:.1%}".format(r, cmc[r - 1]))
-                    torch.cuda.empty_cache()
+                    if dist.get_rank() != 0:
+                        continue
+                # model.eval()
+                acc_ = []
+                for n_iter, (img, vid, camid, camids, target_view, _) in enumerate(val_loader):
+                    with torch.no_grad():
+                        img = img.to(device)
+                        vid = vid.to(device)
+                        camids = camids.to(device)
+                        target_view = target_view.to(device)
+                        feat, score = model(img, cam_label=camids, view_label=target_view)
+                        evaluator.update((feat, vid, camid))
+                        if isinstance(score, list):
+                            acc = (score[0].max(1)[1] == vid).float().mean()
+                        else:
+                            acc = (score.max(1)[1] == vid).float().mean()
+                        acc_.append(acc)
+                cmc, mAP, _, _, _, _, _ = evaluator.compute()
+                acc_ = sum(acc_)/len(acc_)
+                logger.info("Validation Results - Epoch: {}".format(epoch))
+                logger.info("mAP: {:.1%}".format(mAP))
+                mlflow.log_metric(key="val/CMC", value=cmc, step=steps)
+                mlflow.log_metric(key="val/mAP", value=mAP, step=steps)
+                mlflow.log_metric(key="val/acc", value=acc_, step=steps)
+                for r in [1, 5, 10]:
+                    logger.info("CMC curve, Rank-{:<3}:{:.1%}".format(r, cmc[r - 1]))
+                torch.cuda.empty_cache()
 
         mlflow.pytorch.log_model(model, "barely_trained_model")
         mlflow.end_run()
